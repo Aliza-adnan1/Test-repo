47445d2040bf0a76409feb9dff229421b2632dd6
Aliza-adnan1 <106335017+Aliza-adnan1@users.noreply.github.com>
2023-02-24 16:46:22 +0500

    Create wallet.py

---
+++ wallet.py
@@ -0,0 +1,8 @@
+class Wallet:
+  balance=0
+  def setBalance(self,i):
+    self.balance=i
+  def getBalance(self):
+    return self.balance
+  def removeBalance(self,i):
+    self.balance=self.balance-
